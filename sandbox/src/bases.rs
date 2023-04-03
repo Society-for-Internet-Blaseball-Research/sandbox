a13ac69327808ccae3d8a58efa0fa6bbd8d50dc4
dork <38471874+qt-dork@users.noreply.github.com>
2023-04-03 06:36:41 -0500

    just some (probably bad) stuff

--- sandbox/src/bases.rs
+++ sandbox/src/bases.rs
@@ -1,4 +1,7 @@
+use std::{iter::zip, borrow::Borrow};
+
 use uuid::Uuid;
+use itertools::Itertools;
 
 #[derive(Debug, Clone)]
 pub struct Baserunner {
@@ -6,20 +9,23 @@ pub struct Baserunner {
     pub base: u8,
 }
 
+/// Stores where all the baserunners are
 #[derive(Debug, Clone)]
 pub struct Baserunners {
-    pub runners: Vec<Baserunner>,
+    pub ids: Vec<Uuid>,
+    pub bases: Vec<u8>,
 }
 
 impl Baserunners {
     pub fn new() -> Baserunners {
         Baserunners {
-            runners: Vec::new(),
+            ids: Vec::new(),
+            bases: Vec::new(),
         }
     }
 
     pub fn occupied(&self, base: u8) -> bool {
-        self.runners.iter().any(|x| x.base == base)
+        self.bases.iter().any(|&x| x == base)
     }
 
     pub fn can_advance(&self, base: u8) -> bool {
@@ -27,44 +33,48 @@ impl Baserunners {
     }
 
     pub fn at(&self, base: u8) -> Option<Uuid> {
-        self.runners
-            .iter()
-            .find(|x| x.base == base)
-            .map(|x| x.id.clone())
+        self.iter().find(|x| x.base == base).map(|x| x.id)
     }
 
     pub fn advance(&mut self, base: u8) {
-        for r in self.runners.iter_mut() {
-            if r.base == base {
-                r.base += 1;
-                return;
+        for &mut r in self.bases.iter_mut() {
+            if r == base {
+                r += 1;
             }
         }
     }
 
     pub fn remove(&mut self, base: u8) -> Option<Uuid> {
         let idx = self
-            .runners
+            .bases
             .iter()
             .enumerate()
-            .find(|x| x.1.base == base)
+            .find(|(_, &x)| x == base)
             .map(|x| x.0);
 
         if let Some(idx) = idx {
-            let runner = self.runners.remove(idx);
-            Some(runner.id)
+            self.bases.remove(idx);
+            let runner = self.ids.remove(idx);
+            Some(runner)
         } else {
             None
         }
     }
 
     pub fn advance_all(&mut self, amount: u8) {
-        for r in self.runners.iter_mut() {
-            r.base += amount;
+        for &mut r in self.bases.iter_mut() {
+            r += amount;
         }
     }
 
     pub fn walk(&mut self) {
+        let sorted = self.iter_mut().sorted_by(|a, b| Ord::cmp(a.1, b.1));
+        let mut index = 0;
+        for i in sorted {
+            if *i.base == index {
+                index += 1;
+            }
+        }
         // todo: this code is also crap
         let mut num_occupied = 0;
         for i in 0..5 {
@@ -80,7 +90,7 @@ impl Baserunners {
         }
     }
 
-    pub fn advance_if(&mut self, mut f: impl FnMut(&Baserunner) -> bool) {
+    pub fn advance_if(&mut self, mut f: impl FnMut((&)) -> bool) {
         for i in 0..self.runners.len() {
             if self.can_advance(self.runners[i].base) {
                 if f(&self.runners[i]) {
@@ -91,14 +101,15 @@ impl Baserunners {
     }
 
     pub fn add(&mut self, base: u8, id: Uuid) {
-        self.runners.push(Baserunner { id, base });
+        self.ids.push(id);
+        self.bases.push(base);
     }
 
     pub fn iter(&self) -> impl Iterator<Item = &Baserunner> {
-        self.runners.iter()
+        zip(self.ids.iter(), self.bases.iter()).map(|x| Baserunner{id: x.0, base: x.1})
     }
 
     pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut Baserunner> {
-        self.runners.iter_mut()
+        zip(self.ids.iter_mut(), self.bases.iter_mut()).map(|x| Baserunner{id: x.0, base: x.1})
     }
 }
