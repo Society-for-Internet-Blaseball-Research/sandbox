90b89d560ec6d742ba285ac97f12231d5fe369ce
LmaoLynx <chopikashvilid@gmail.com>
2024-08-14 18:11:26 +0300

    Fix implementing Red Hot completely incorrectly

--- sandbox/src/formulas.rs
+++ sandbox/src/formulas.rs
@@ -15,11 +15,11 @@ pub fn swing_threshold(pitcher: &Player, batter: &Player, is_strike: bool) -> f6
         let combined_batting = (batter.divinity
             + batter.musclitude
             + (1.0 - batter.patheticism)
-            + multiplier(PlayerAttr::Thwackability, batter.thwackability, &batter.mods))
+            + batter.thwackability * multiplier(PlayerAttr::Thwackability, &batter.mods))
             / 4.0;
         0.7 + 0.35 * combined_batting - 0.4 * pitcher.ruthlessness + 0.2 * (visc - 0.5)
     } else {
-        let combined = (12.0 * batter.ruthlessness - 5.0 * multiplier(PlayerAttr::Moxie, batter.moxie, &batter.mods) //batter ruth? are we sure
+        let combined = (12.0 * batter.ruthlessness - 5.0 * batter.moxie * multiplier(PlayerAttr::Moxie, &batter.mods) //batter ruth? are we sure
             + 5.0 * batter.patheticism
             + 4.0 * visc)
             / 20.0;
@@ -38,7 +38,7 @@ pub fn contact_threshold(pitcher: &Player, batter: &Player, is_strike: bool) -> 
 
     if is_strike {
         let combined_batting =
-            (batter.divinity + batter.musclitude + multiplier(PlayerAttr::Thwackability, batter.thwackability, &batter.mods) - batter.patheticism) / 2.0;
+            (batter.divinity + batter.musclitude + batter.thwackability * multiplier(PlayerAttr::Thwackability, &batter.mods) - batter.patheticism) / 2.0;
 
         (0.78 - 0.08 * ruth + 0.16 * ballpark_sum + 0.17 * combined_batting.powf(1.2)).min(0.925)
     } else {
@@ -50,7 +50,7 @@ pub fn contact_threshold(pitcher: &Player, batter: &Player, is_strike: bool) -> 
 pub fn foul_threshold(_pitcher: &Player, batter: &Player) -> f64 {
     let fwd = 0.5;
     let obt = 0.5;
-    let batter_sum = (batter.musclitude + multiplier(PlayerAttr::Thwackability, batter.thwackability, &batter.mods) + batter.divinity) / 3.0;
+    let batter_sum = (batter.musclitude + batter.thwackability * multiplier(PlayerAttr::Thwackability, &batter.mods) + batter.divinity) / 3.0;
     0.25 + 0.1 * fwd - 0.1 * obt + 0.1 * batter_sum
 }
 
@@ -62,7 +62,7 @@ pub fn out_threshold(pitcher: &Player, batter: &Player, defender: &Player) -> f6
     let visc_center = 0.0;
     let fwd_center = 0.0;
 
-    let thwack = multiplier(PlayerAttr::Thwackability, batter.thwackability, &batter.mods); // with vibes
+    let thwack = batter.thwackability * multiplier(PlayerAttr::Thwackability, &batter.mods); // with vibes
     let unthwack = pitcher.unthwackability; // with vibes
     let omni = defender.omniscience; // with vibes
 
@@ -198,16 +198,14 @@ pub fn flyout_advancement_threshold(runner: &Player, base_from: u8) -> f64 {
     }
 }
 
-fn multiplier(attr: PlayerAttr, value: f64, mods: &Mods) -> f64 {
+fn multiplier(attr: PlayerAttr, mods: &Mods) -> f64 {
+    let mut multiplier = 1.0;
     if mods.has(Mod::RedHot) {
         if let PlayerAttr::Thwackability = attr {
-            value + 0.3
+            multiplier += 4.0;
         } else if let PlayerAttr::Moxie = attr {
-            value + 0.1
-        } else {
-            value
-        }
-    } else {
-        value
+            multiplier += 2.0;
+        } 
     }
+    multiplier
 }
